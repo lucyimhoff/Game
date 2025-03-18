4478c9a626c241a984e8588e1081c4cb708519bc
lucyimhoff <lucyi4@nycstudents.net>
2025-03-18 13:02:28 +0000

    finished

--- Main.java
+++ Main.java
@@ -17,5 +17,29 @@ public class Main{
         two.getLevel(2).setPoints(100);
         two.getLevel(3).setPoints(500);
         System.out.println(two.getScore());
+
+
+        Game three = new Game();
+        three.getLevel(1).reachGoal();
+        three.getLevel(1).setPoints(200);
+        three.getLevel(2).setPoints(100);
+        three.getLevel(3).goalReached();
+        three.getLevel(3).setPoints(500);
+        three.makeBonus();
+        System.out.println(three.getScore());
+
+
+        Game four = new Game();
+        four.getLevel(1).setPoints(200);
+        four.getLevel(2).goalReached();
+        four.getLevel(2).setPoints(100);
+        four.getLevel(3).goalReached();
+        four.getLevel(3).setPoints(500);
+        System.out.println(four.getScore());
+
+        Game five = new Game();
+        System.out.println(five.playManyTimes(4));
     }
+
+
 }
